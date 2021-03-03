21021ec476f779f333210d75411a769d769fa629
MaideAkdede <maide.akdede@student.hepl.com>
2021-03-03 17:07:31 +0100

    :egg: commit changes

--- src/app.js
+++ src/app.js
@@ -1,47 +1,39 @@
-const canvas = document.querySelector('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-const circles = [];
-const circlesCount = 100;
-const ctx = canvas.getContext('2d');
-
-const circle = {
-    speed : {x: (2 + Math.floor(Math.random() * 5)), y: (2 + Math.floor(Math.random() * 5))},
-    position : {x: canvas.width / 2, y: canvas.height / 2},
-    radius : canvas.width / 10,
-    colors : ['#9C7785', '#474457', '#DFCDDD', '#E8B452', '#D6384F'],
-    color : '',
-    init(){
-        this.color = this.colors.sort(() => 0.5 - Math.random())[0];
+import Circle from "./js/Circle";
+const animation = {
+    canvas: document.querySelector('canvas'),
+    circles: [],
+    circlesCount: 10,
+    ctx: null,
+    mouse  :{
+        x : undefined,
+        y : undefined,
+        zoneSize : 50
+    },
+    init() {
+        this.ctx = this.canvas.getContext('2d');
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+        for (let i = 0; i < this.circlesCount; i++) {
+            this.circles.push(new Circle(animation));
+        }
+        window.addEventListener('resize', () => {
+            this.canvas.width = window.innerWidth;
+            this.canvas.height = window.innerHeight;
+        });
+        window.addEventListener('mousemove', event => {
+            this.mouse.x = event.x;
+            this.mouse.y = event.y;
+        })
         this.animate();
     },
-    draw(){
-        ctx.fillStyle = this.color;
-        ctx.beginPath();
-        ctx.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI);
-        ctx.fill();
-    },
     animate(){
-        if (this.position.y + this.radius > canvas.height || this.position.y - this.radius < 0) {
-            this.speed.y = -this.speed.y
-        }
-        if (this.position.x + this.radius > canvas.width || this.position.x - this.radius < 0) {
-            this.speed.x = -this.speed.x
-        }
-        this.position.x += this.speed.x;
-        this.position.y += this.speed.y;
-
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        this.draw();
-        window.requestAnimationFrame(()=>{
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.circles.forEach(circle=>{
+            circle.update()
+        })
+        window.requestAnimationFrame(() => {
             this.animate();
         })
     }
 }
-circle.init();
-
-window.addEventListener('resize', () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-})
+animation.init();
